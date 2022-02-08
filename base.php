149b81fe8c8b363d96bd56dcbced2f51edc99410
baptistemahieu <baptiste.mahieu@edu.ece.fr>
2022-02-08 12:52:32 +0100

    test VSC Co-authored-by: loic chaligne <xynave@users.noreply.github.com>

---
+++ base.php
@@ -0,0 +1,29 @@
+<!-- page accueil -->
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <title>Présentation du BTS Forge</title>
+
+</head>
+
+<body>
+
+
+    <!--==================Début de la barre==========================-->
+
+    <!--==================Fin de l'image de fin'==========================-->
+    <div >
+        
+                   <h1> baptiste la pute<h1>
+                   <?php include("page2.php"); ?>
+        </div>
+    
+</body>
+
+</html>
---
+++ page2.php
@@ -0,0 +1,3 @@
+<div>
+    <p> yes yes!!!!</p>
+</div>
