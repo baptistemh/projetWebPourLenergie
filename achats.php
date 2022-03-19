cba1a2b5a12d66874f0f15a67b5d6bee11eb9937
baptistemahieu <baptiste.mahieu@edu.ece.fr>
2022-03-19 15:46:20 +0100

    pages d'ahats V1

--- achats.php
+++ achats.php
@@ -17,10 +17,32 @@ if (!$conn) {
 
 echo "Connexion réussie"."<br/>";
 
-
-  
-
+$requete = "SELECT count(*) FROM item where id = '1' ";
+        $exec_requete = mysqli_query($conn,$requete);
+        $reponse      = mysqli_fetch_array($exec_requete);
+        $count = $reponse['count(*)'];
+        if($count!=0) // nom d'utilisateur et mot de passe correctes
+        {
+          $requete2 = "SELECT * FROM item where id = '1' ";
+          $exec_requete2 = mysqli_query($conn,$requete2);
+          $reponse2     = mysqli_fetch_object($exec_requete2);
+          $nbItem1 = $reponse2->nbItem1;
+          $nbItem2 = $reponse2->nbItem2;
+          $nbItem3 = $reponse2->nbItem3;
+        }
 echo $nbItem1;
+
+/*if($_POST['pourItem1']){
+   
+    $sql = "UPDATE item SET nbItem1 = '$item1', nbItem2 = '$item2', nbItem3 = '$item3' where id = '1' ";
+    if (mysqli_query($conn, $sql)) {
+          echo "Nouvelle modification créé avec succès";
+          $sql = "SELECT nbItem1, nbItem2, nbItem3 from item";
+    } else {
+          echo "Erreur : " . $sql . "<br>" . mysqli_error($conn);
+    }
+    echo "c'est bon";
+    }*/
 
 mysqli_close($conn);
 
@@ -35,21 +57,21 @@ mysqli_close($conn);
 		<div id="f1">
 			<center>
 				<img src="Thib.jpeg" width="150" height="150">
-				<button value="bouquet" id="one" onclick="plusF1()">ITEM 1<br>2€<br><?php echo $nbItem1; ?></button>
+				<button value="bouquet" id="one" onclick="plusF1()">ITEM 1<br>2€ il en reste <?php echo $nbItem1; ?></button>
 			</center>
 		</div>
 
 		<div id="f2">
 			<center>
 				<img src="Charlotte.jpeg" width="150" height="150">
-				<button value="bouquet" id="two" onclick="plusF2()">ITEM 2<br>2€<br></button>
+				<button value="bouquet" id="two" onclick="plusF2()">ITEM 2<br>2€ il en reste <?php echo $nbItem2; ?></button>
 			</center>
 		</div>
 		
 		<div id="f3">
 			<center>
 				<img src="Baptiste.jpeg" width="150" height="150">
-				<button value="bouquet" id="three" onclick="plusF3()">ITEM 3<br>2€</button>
+				<button value="bouquet" id="three" onclick="plusF3()">ITEM 3<br>2€ il en reste <?php echo $nbItem3; ?></button>
 		</center>
 		</div>
 
@@ -58,16 +80,24 @@ mysqli_close($conn);
 </div>
    		<div id="footer">
      	<button onclick="reset()"> RESET </button>
-     	Total price : <span id="sum"></span> €
-         <button onclick="reset()"> VALIDATE </button>
+     	Total price : <span id="sum"></span> € ! Il reste : <span id="resteItem1"></span> item1, <span id="resteItem2"></span> item2 et <span id="resteItem3"></span> item3
+         <input type="submit" id='submit' name="button2" value='VALIDATE' >
      </div>
      	
 
 
-<script type="text/javascript">
+     <script type="text/javascript">
 		var f1=0;
 		var f2=0;
 		var f3=0;
+        var nombreItem1=<?php echo $nbItem1; ?>;
+        var nombreItem2=<?php echo $nbItem2; ?>;
+        var nombreItem3=<?php echo $nbItem3; ?>;
+
+        /*function moinsUn() { 
+            nombreItem1 = nombreItem1 - 1;
+            document.getElementById("test").innerHTML=nombreItem1;
+        }*/
 
 		function total(){
 			var tot = f1+f2+f3; 
@@ -80,23 +110,39 @@ mysqli_close($conn);
 			f1=0;
 			f2=0;
 			f3=0;
+            var nombreItem1=<?php echo $nbItem1; ?>;
+        var nombreItem2=<?php echo $nbItem2; ?>;
+        var nombreItem3=<?php echo $nbItem3; ?>;
 			document.getElementById("sum").innerHTML=0;
+            document.getElementById("resteItem1").innerHTML=<?php echo $nbItem1; ?>;
+            document.getElementById("resteItem2").innerHTML=<?php echo $nbItem2; ?>;
+            document.getElementById("resteItem3").innerHTML=<?php echo $nbItem3; ?>;
 		}
 
 		function plusF1(){
 			f1+=2;
+            nombreItem1 = nombreItem1 - 1;
+            document.getElementById("resteItem1").innerHTML=nombreItem1;
 			total();
+            <?php
+            echo "ca marche";
+            ?>
+            
 		}
 		function plusF2(){
 			f2+=2;
+            nombreItem2 = nombreItem2 - 1;
+            document.getElementById("resteItem2").innerHTML=nombreItem2;
 			total();
 		}
 		function plusF3(){
 			f3+=2;
+            nombreItem3 = nombreItem3 - 1;
+            document.getElementById("resteItem3").innerHTML=nombreItem3;
 			total();
 		}
-
 	</script>
 
 </body>
+
 </html>
--- manageItem.php
+++ manageItem.php
@@ -5,7 +5,7 @@ body{
     background: #11181C;
 }
 #container{
-    width:400px;
+    width:600px;
     margin:0 auto;
     margin-top:10%;
 }
@@ -18,7 +18,7 @@ form {
     box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
 }
 #container h1{
-    width: 38%;
+    width: 68%;
     margin: 0 auto;
     padding-bottom: 10px;
 }
@@ -56,26 +56,53 @@ input[type=submit]:hover {
        <meta charset="utf-8">
     </head>
     <body>
+    <?php
+
+$item1 = $_POST["item1"];
+$item2 = $_POST["item2"];
+$item3 = $_POST["item3"];
+
+
+
+$conn = mysqli_connect("localhost", "root", "root", "connexion");
+
+if (!$conn) {
+    die("Échec de la connexion : " . mysqli_connect_error());
+}
+
+echo "Connexion réussie"."<br/>";
+
+if($_POST['button2']){
+   
+$sql = "UPDATE item SET nbItem1 = '$item1', nbItem2 = '$item2', nbItem3 = '$item3' where id = '1' ";
+if (mysqli_query($conn, $sql)) {
+      echo "Nouvelle modification créé avec succès";
+      $sql = "SELECT nbItem1, nbItem2, nbItem3 from item";
+} else {
+      echo "Erreur : " . $sql . "<br>" . mysqli_error($conn);
+}
+}
+
+
+mysqli_close($conn);
+
+?>
         <div id="container">
             <!-- zone de connexion -->
             
-            <form action="verif.php" method="POST">
-                <h1>Connexion</h1>
+            <form method="POST">
+                <h1>Manage the Number of Items</h1>
                 
-                <label><b>E-mail</b></label>
-                <input type="text" placeholder="Enter your e-mail" name="input2" required>
+                <label><b>Number of Item 1</b></label>
+                <input type="text" placeholder="Enter the number" name="item1">
 
-                <label><b>Password</b></label>
-                <input type="password" placeholder="Enter your password" name="input3" required>
+                <label><b>Number of Item 2</b></label>
+                <input type="text" placeholder="Enter the number" name="item2">
 
-                <input type="submit" id='submit' value='LOGIN' >
-                <?php
-                if(isset($_GET['erreur'])){
-                    $err = $_GET['erreur'];
-                    if($err==1 || $err==2)
-                        echo "<p style='color:red'>E-mail or password incorrect</p>";
-                }
-                ?>
+                <label><b>Number of Item 3</b></label>
+                <input type="text" placeholder="Enter the number" name="item3">
+
+                <input type="submit" id='submit' name="button2" value='VALIDATE' >
             </form>
         </div>
     </body>
